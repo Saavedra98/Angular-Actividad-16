cb4c5dfc3963cead53510b1d53b8039d7852162d
Saavedra98 <saavedraerazoc@gmail.com>
2023-01-11 14:53:49 -0500

    listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { UruguayComponent } from './pages/uruguay/uruguay.component';
+import { ChileComponent } from './pages/chile/chile.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeruComponent } from './pages/peru/peru.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:PeruComponent},
+  {path:'chile', component:ChileComponent},
+  {path:'uruguay', component:UruguayComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
